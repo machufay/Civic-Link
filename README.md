7f08784920e5caf88ed05485d18671e9e589a840
machufay <mattyangfei@gmail.com>
2025-10-05 18:49:32 -0500

    hi

--- README.md
+++ README.md
@@ -11,3 +11,7 @@ A website aimed to let city engineers post construction updates about their comm
 - engineer login page: form.html
 
 - map page: map.html
+
+
+https://docs.google.com/spreadsheets/d/1UK356MwkhS73fkZHGkeQwS_UBx9YpTf9dIunNaGbXLw/edit?gid=0#gid=0
+
---
+++ form.js
@@ -0,0 +1,89 @@
+  const API_KEY = "AIzaSyAoYSJ-B-i4r-jgl0n3is3VJMgBhVhwy44";
+  const SHEET_ID = "1UK356MwkhS73fkZHGkeQwS_UBx9YpTf9dIunNaGbXLw";
+
+  let ptable; //just values, no meta-data
+    //prototype table in google sheet format, later turned into json format
+  let table;
+  let titles; 
+    //names of all object parameters 
+
+main()
+ async function main(){
+
+  await loadData();
+  table = await STO(ptable);
+  //do not mess w awaits here
+  showdata()
+
+  document.getElementById("submitid").addEventListener("click", function(){
+
+    const age = document.getElementById("ageid").value;
+    const name = document.getElementById("nameid").value;
+    const date = document.getElementById("dateid").value;
+    const location = document.getElementById("locationid").value;
+    console.log(location);
+    submitData(name, age, date, location);
+  });
+
+}
+
+async function submitData(name, age, date, location) {
+  const data = { name, age, date, location};
+  const response = await fetch(
+    "https://script.google.com/macros/s/AKfycbyn_fJ3jlHz4OiKMP4vKbH1xN5b00H1GujO35r8FWykwFY2ZxqbOK-wt-ZwCllzu4FQZQ/exec",
+    {
+      method: "POST",
+      mode: "no-cors",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify(data)
+    }
+  );
+  console.log("Data sent!");
+}
+
+function showdata(){
+  const newEntry = document.createElement("ul")
+
+  table.forEach((obj,i) => {
+    const newLine = document.createElement("li");
+
+      ptable[0].forEach(para =>{
+      newLine.textContent += (table[i][para]+ ', ');
+      });
+    newEntry.appendChild(newLine);
+  });
+  document.getElementById("userlist").appendChild(newEntry);
+}
+
+async function loadData() {
+    const url = `https://sheets.googleapis.com/v4/spreadsheets/${SHEET_ID}/values/Sheet1?key=${API_KEY}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    ptable = data.values;
+    //console.log(data.values); // array of rows
+  }
+
+async function finddata(){
+
+}
+
+function STO(values){
+  const headers = values[0]; //headers are now an array of object titles 
+  const rows = values.slice(1); //stop at 1
+
+ return rows.map((row) => { 
+    
+    const obj={}; //self explanatory
+
+    headers.forEach((header,i) => {
+    obj[header] = row[i] ?? ""; //exclude if Null or undef
+    });
+
+    return obj;
+  });
+}
+
+
+
